6bd82d5f885cfdb12147974c330e91983f15e70a
Yeasir-Hossain <yeasir06@gmail.com>
2022-04-18 13:15:13 +0600

    Components added and route set

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Blogs from './Components/Blogs/Blogs';
+import Checkout from './Components/Checkout/Checkout';
 import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
+import NotFound from './Components/NotFound/NotFound';
+import RequireAuth from './Components/RequireAuth/RequireAuth';
+import ServiceDetail from './Components/ServiceDetail/ServiceDetail';
+import SignUp from './Components/SignUp/SignUp';
+import About from './Pages/About/About';
+
+
+
+
+
+
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path='/service/:serviceId' element={<ServiceDetail></ServiceDetail>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/about" element={<Blogs></Blogs>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/signup" element={<SignUp></SignUp>}></Route>
+        <Route path="/checkout" element={
+          <RequireAuth>
+            <Checkout></Checkout>
+          </RequireAuth>
+        }></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
+      </Routes>
       <Footer></Footer>
     </div>
   );
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import './Footer.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCopyright } from '@fortawesome/free-solid-svg-icons'
 
 const Footer = () => {
     const today = new Date();
     const year = today.getFullYear();
     return (
-        <footer className='text-center text-white bg-dark mt-5 footer'>
-            <p><small>copyright @ {year} </small></p>
+        <footer className='text-center text-white bg-dark mt-5 footer d-flex flex-column justify-content-center align-items-center'>
+            <p className='mb-0'>All rights reserved</p>
+            <p><small>Copyright <FontAwesomeIcon icon={faCopyright} /> {year} </small></p>
         </footer>
     );
 };
